6101eace59a12556f117a05c1c60e730f773a936
holistudio <daniel.bin.lu@gmail.com>
2025-09-29 12:11:59 -0400

    Update dance.py
    parse_move() now checks a dictionary of stored parsed moves
     - if the move string is already in the dictionary keys, quickly return move type and parameters
     - store_move() adds a newly encountered move otherwise

--- 2017/16/dance.py
+++ 2017/16/dance.py
@@ -1,4 +1,10 @@
-
+parsed_moves = {}
+# parsed_moves = {
+#   'x1/3': {
+#       "move_type": "x",
+#       "parameters": (1,3)
+#   }
+# }
 
 def spin(lineup: list, X: int):
     '''Counting from the end, grab X programs from the end and move to front'''
@@ -31,29 +37,47 @@ def partner(lineup: list, a: str, b: str):
     lineup = exchange(lineup, location_a, location_b)
     return lineup
 
+def store_move(move: str, move_type: str, parameters: tuple):
+    parsed_moves[move] = {
+        "move_type": move_type,
+        "parameters": parameters
+    }
+    pass
+
 def parse_move(move: str):
     # NOTE: Revisit
     # - can we reduce the number of if statement checks?
     # - how do we not repeated parse the same command?
-    # get move type based on the first letter
-    move_type = move[0]
 
-    # create a list of parameters based on move_type
-    if move_type == 's':
-        X = int(move[1:])
-        return move_type, X
-    elif move_type == 'x':
-        move_components = move[1:]
-        A = int(move_components.split('/')[0])
-        B = int(move_components.split('/')[1])
-        return move_type, (A, B)
-    elif move_type == 'p':
-        move_components = move[1:]
-        A = move_components.split('/')[0]
-        B = move_components.split('/')[1]
-        return move_type, (A, B)
+    # check parsed_moves has the move string already
+    if move in parsed_moves.keys():
+        print("WE'VE BEEN HERE!")
+        move_type = parsed_moves[move]["move_type"]
+        parameters = parsed_moves[move]["parameters"]
+        return move_type, parameters
     else:
-        return ValueError('Dance move not recognized...')
+        # get move type based on the first letter
+        move_type = move[0]
+
+        # create a list of parameters based on move_type
+        if move_type == 's':
+            X = int(move[1:])
+            store_move(move, move_type, (X))
+            return move_type, X
+        elif move_type == 'x':
+            move_components = move[1:]
+            A = int(move_components.split('/')[0])
+            B = int(move_components.split('/')[1])
+            store_move(move, move_type, (A, B))
+            return move_type, (A, B)
+        elif move_type == 'p':
+            move_components = move[1:]
+            A = move_components.split('/')[0]
+            B = move_components.split('/')[1]
+            store_move(move, move_type, (A, B))
+            return move_type, (A, B)
+        else:
+            return ValueError('Dance move not recognized...')
 
 def test_spin(lineup: list, move: str):
     print(f'Original\n{lineup}')
@@ -116,10 +140,13 @@ if __name__ == "__main__":
     # line = test_exchange(line, 'x3/4')
     # line = test_partner(line, 'pe/b')
 
-    # programs = ['a','b','c','d','e']
+    programs = ['a','b','c','d','e']
+    moves = ['s1', 'x3/4', 'pe/b']
+    moves = ['s1', 'x3/4', 'pe/b', 'x3/4']
+
+    # programs = [chr(x) for x in range(97,97+16)]
     # moves = ['s1', 'x3/4', 'pe/b']
 
-    programs = [chr(x) for x in range(97,97+16)]
-    moves = ['s1', 'x3/4', 'pe/b']
-
     main(programs, moves)
+
+    print(parsed_moves)
