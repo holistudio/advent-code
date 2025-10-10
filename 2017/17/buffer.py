8355bbe218fa7002770b87aa11eafe34964f2011
holistudio <daniel.bin.lu@gmail.com>
2025-10-10 14:31:48 -0400

    Update buffer.py
    Solved part 2!

--- 2017/17/buffer.py
+++ 2017/17/buffer.py
@@ -13,37 +13,54 @@ def insert_ix(buffer_len, i, x):
             i += 1
         else:
             i = 0
-        # print(i)
+    # if i == 0:
+    #     print(buffer_len, x, buffer_len // x, buffer_len % x)
     return i
     
 def main(steps, max_value):
     # starts with a circular buffer containing only the value `0`
     buffer = [0]
+    len_buffer = 1
 
     ix = 0
+
+    after_zero = 0
+
+    # insert_ixs = []
 
     for v in range(1, max_value+1):
         # print(f'ix_0 = {ix}')
 
         # step forward circular buffer in _ steps
-        ix = insert_ix(len(buffer), ix+1, steps)
+        ix = insert_ix(len_buffer, ix+1, steps)
         # print(f'ix_1 = {ix}')
+        # insert_ixs.append(ix+1)
 
         # insert value at current position
         # split the buffer into two
-        if len(buffer) > 1:
-            left_half = buffer[:ix+1]
-            right_half = buffer[ix+1:]
-        else:
-            left_half = buffer
-            right_half = []
+        # if len(buffer) > 1:
+        #     left_half = buffer[:ix+1]
+        #     right_half = buffer[ix+1:]
+        # else:
+        #     left_half = buffer
+        #     right_half = []
 
         # recombine
-        buffer = left_half + [v] + right_half
+        # buffer = left_half + [v] + right_half
+
+        # if buffer[1] != after_zero:
+        #     print(buffer[1])
+        #     after_zero = buffer[1]
+        if ix == 0:
+            print(v)
+        len_buffer += 1
 
     # print(f"Final Buffer:  {buffer}")
-    print(f"Last Inserted: {buffer[ix+1]}")
-    print(f"Value After:   {buffer[ix+2]}")
+    # print(f"Last Inserted: {buffer[ix+1]}")
+    # print(f"Value After:   {buffer[ix+2]}")
+    # print(insert_ixs[:20])
 
 if __name__ == "__main__":
-    main(steps=355, max_value=2017)
+    main(steps=355, max_value=50000000)
+    # main(steps=355, max_value=2017)
+    # main(steps=3, max_value=2017)
