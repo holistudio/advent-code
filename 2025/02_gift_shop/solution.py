aeb51a38270f23e5beadb2ae4900b9f418806ef3
holistudio <daniel.bin.lu@gmail.com>
2025-12-07 17:05:28 -0500

    Update solution.py

--- 2025/02_gift_shop/solution.py
+++ 2025/02_gift_shop/solution.py
@@ -24,35 +24,30 @@ def part2(input_list):
         range_strs = range_item.split('-')
         start_id, end_id = int(range_strs[0]), int(range_strs[1])
         for id in range(start_id, end_id+1):
-            tried_seqs = []
             id_str = str(id)
-            # print(id)
-            # get id sequence length
             seq_len = len(id_str)
 
+            tried_seqs = []
             sliding_window_len = 1
             found = False
             while not found and (sliding_window_len <= (seq_len // 2)):
                 repeats = seq_len // sliding_window_len
                 for i in range(0,seq_len-sliding_window_len):
                     sub_id = id_str[i:i+sliding_window_len]
-                    # print(sub_id)
                     if sub_id not in tried_seqs:
                         tried_seqs.append(sub_id)
                         candidate_seq = ''
                         for j in range(repeats):
                             candidate_seq += sub_id
                         if candidate_seq == id_str:
-                            # print(candidate_seq)
                             sum += id
                             found = True
                             break
-                        
                 sliding_window_len +=1
-            # print()
     return sum
 
 if __name__ == '__main__':
     with open('input.txt') as f:
         input_list = f.read().split(',')
-    print(part2(input_list))
+    print(f"Part 1 Answer: {part1(input_list)}")
+    print(f"Part 2 Answer: {part2(input_list)}")
