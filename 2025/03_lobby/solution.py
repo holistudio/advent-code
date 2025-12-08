322077e5d52b85d7768ff1052f446df44d910ed4
holistudio <daniel.bin.lu@gmail.com>
2025-12-07 20:57:52 -0500

    part 2 passes example

--- 2025/03_lobby/solution.py
+++ 2025/03_lobby/solution.py
@@ -11,7 +11,29 @@ def part1(batteries):
         total_joltage += max_joltage
     return total_joltage
 
+def part2(batteries):
+    total_joltage = 0
+    for bank in batteries:
+        print(bank)
+        ints = [int(c) for c in bank]
+        
+        n = 12
+        max_joltage = ''
+        while n > 0:
+            ix = len(ints)
+            while len(ints[ints.index(max(ints[:ix])):])<n:
+                ix -= 1
+            max_digit = str(max(ints[:ix]))
+            max_joltage += max_digit
+            n -= 1
+            ints = ints[ints.index(max(ints[:ix]))+1:]
+        print(max_joltage)
+        print()
+        total_joltage += int(max_joltage)
+    return total_joltage
+
 if __name__ == '__main__':
-    with open('input.txt') as f:
+    with open('example.txt') as f:
         batteries = f.read().split('\n')
-    print(part1(batteries))
+    # print(part1(batteries))
+    print(part2(batteries))
