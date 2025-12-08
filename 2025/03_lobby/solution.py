34ff99f342e47072b4311f7ce0856c7d01bea824
holistudio <daniel.bin.lu@gmail.com>
2025-12-07 20:37:14 -0500

    Update solution.py

--- 2025/03_lobby/solution.py
+++ 2025/03_lobby/solution.py
@@ -3,16 +3,13 @@ from collections import OrderedDict
 def part1(batteries):
     total_joltage = 0
     for bank in batteries:
-        print(bank)
         ints = [int(c) for c in bank]
         ix = len(ints)
-        while len(ints[ints.index(max(ints[:ix])):])==1:
+        if len(ints[ints.index(max(ints[:ix])):])==1:
             ix -= 1
         max_digit = str(max(ints[:ix]))
         next_max_digit = str(max(ints[ints.index(max(ints[:ix]))+1:]))
         max_joltage = int(max_digit + next_max_digit)
-        print(max_joltage)
-        print()
         total_joltage += max_joltage
     return total_joltage
 
