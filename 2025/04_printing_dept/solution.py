2e6f4c0a88d919db5c5248e249410ab28485e9c5
holistudio <daniel.bin.lu@gmail.com>
2025-12-18 09:54:18 -0500

    Update solution.py
    passes part2 example

--- 2025/04_printing_dept/solution.py
+++ 2025/04_printing_dept/solution.py
@@ -1,4 +1,7 @@
+import copy
+
 def part1(grid):
+    grid_copy = copy.deepcopy(grid)
     H = len(grid)
     W = len(grid[0])
     count = 0
@@ -27,13 +30,23 @@ def part1(grid):
                 window = ''.join(window)
                 if window.count('@') - 1 < 4:
                     # print(i,j, window)
+                    grid_copy[i] = grid_copy[i][:j] + '.' + grid_copy[i][j+1:] 
                     count += 1
-    return count
+    return count, grid_copy
+
+def part2(grid):
+    count, grid = part1(grid)
+    total = count
+    while count > 0:
+        count, grid = part1(grid)
+        total += count
+    return total
 
 
 
 if __name__ == "__main__":
     grid = []
-    with open("input.txt") as f:
+    with open("example.txt") as f:
         grid = f.read().split('\n')
-    print(part1(grid))
+    print(part1(grid)[0])
+    print(part2(grid))
