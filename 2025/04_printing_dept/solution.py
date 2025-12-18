bd1e4c5a1f7012c4daa602f52b8d361de174d2f8
holistudio <daniel.bin.lu@gmail.com>
2025-12-18 10:04:08 -0500

    Update solution.py
    simplify part 1 code

--- 2025/04_printing_dept/solution.py
+++ 2025/04_printing_dept/solution.py
@@ -8,26 +8,20 @@ def part1(grid):
     for i in range(H):
         for j in range(W):
             if grid[i][j] == "@":
-                if (0 < i < H-1) and (0 < j < W-1):
-                    window = [grid[i-1][j-1:j+2], grid[i][j-1:j+2], grid[i+1][j-1:j+2]]
-                else:
-                    if i == 0 and (0 < j < W-1):
-                        window = [grid[i][j-1:j+2], grid[i+1][j-1:j+2]]
-                    if i == H-1 and (0 < j < W-1):
-                        window = [grid[i-1][j-1:j+2], grid[i][j-1:j+2]]
-                    if j == 0 and (0 < i < H-1):
-                        window = [grid[i-1][:j+2], grid[i][:j+2], grid[i+1][:j+2]]
-                    if j == W-1 and (0 < i < H-1):
-                        window = [grid[i-1][j-1:], grid[i][j-1:], grid[i+1][j-1:]]
-                    if i == 0 and j == 0:
-                        window = [grid[i][:j+2], grid[i+1][:j+2]]
-                    if i == 0 and j == W-1:
-                        window = [grid[i][j-1:], grid[i+1][j-1:]]
-                    if i == H-1 and j == 0:
-                        window = [grid[i-1][:j+2], grid[i][:j+2]]
-                    if i == H-1 and j == W-1:
-                        window = [grid[i-1][j-1:], grid[i][j-1:]]
+                row_start = max(0, i-1)
+                row_end = min(H-1, i+1)
+                col_start = max(0, j-1)
+                col_end = min(W-1, j+1)
+
+                window = []
+                for r in range(row_start, row_end+1):
+                    window_row = ""
+                    for c in range(col_start, col_end+1):
+                        window_row += grid[r][c]
+                    window.append(window_row)
+
                 window = ''.join(window)
+                
                 if window.count('@') - 1 < 4:
                     grid_copy[i] = grid_copy[i][:j] + '.' + grid_copy[i][j+1:] 
                     count += 1
