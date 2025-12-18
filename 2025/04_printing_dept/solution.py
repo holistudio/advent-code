9d1a3a2e2193ff6d0f0b894cf90653055b333e15
holistudio <daniel.bin.lu@gmail.com>
2025-12-18 09:44:06 -0500

    Create solution.py
    example works

---
+++ 2025/04_printing_dept/solution.py
@@ -0,0 +1,39 @@
+def part1(grid):
+    H = len(grid)
+    W = len(grid[0])
+    count = 0
+    for i in range(H):
+        for j in range(W):
+            if grid[i][j] == "@":
+                if (0 < i < H-1) and (0 < j < W-1):
+                    window = [grid[i-1][j-1:j+2], grid[i][j-1:j+2], grid[i+1][j-1:j+2]]
+                else:
+                    if i == 0 and (0 < j < W-1):
+                        window = [grid[i][j-1:j+2], grid[i+1][j-1:j+2]]
+                    if i == H-1 and (0 < j < W-1):
+                        window = [grid[i-1][j-1:j+2], grid[i][j-1:j+2]]
+                    if j == 0 and (0 < i < H-1):
+                        window = [grid[i-1][:j+2], grid[i][:j+2], grid[i+1][:j+2]]
+                    if j == W-1 and (0 < i < H-1):
+                        window = [grid[i-1][j-1:], grid[i][j-1:], grid[i+1][j-1:]]
+                    if i == 0 and j == 0:
+                        window = [grid[i][:j+2], grid[i+1][:j+2]]
+                    if i == 0 and j == W-1:
+                        window = [grid[i][j-1:], grid[i+1][j-1:]]
+                    if i == H-1 and j == 0:
+                        window = [grid[i-1][:j+2], grid[i][:j+2]]
+                    if i == H-1 and j == W-1:
+                        window = [grid[i-1][j-1:], grid[i][j-1:]]
+                window = ''.join(window)
+                if window.count('@') - 1 < 4:
+                    # print(i,j, window)
+                    count += 1
+    return count
+
+
+
+if __name__ == "__main__":
+    grid = []
+    with open("example.txt") as f:
+        grid = f.read().split('\n')
+    print(part1(grid))
